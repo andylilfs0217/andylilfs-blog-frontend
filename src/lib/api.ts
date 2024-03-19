d31ce459f7bfc5d8b6dd85e94ab7c3ff83ada5a8
Andy Li <andylilfs@gmail.com>
2024-03-18 22:31:38 -0400

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,17 @@
 import { Post } from "@/interfaces/post";
 
 const ANDYLILFS_BLOG_BACKEND_API_URL = process.env.ANDYLILFS_BLOG_BACKEND_API_URL;
+const REVALIDATE_TIME = parseInt(process.env.REVALIDATE_TIME ?? "60");
 
 export async function getPostById(id: string) {
-  const response = await fetch(`${ANDYLILFS_BLOG_BACKEND_API_URL}/blogs/${id}`, { cache: 'no-store' });
+  const response = await fetch(`${ANDYLILFS_BLOG_BACKEND_API_URL}/blogs/${id}`, { next: { revalidate: REVALIDATE_TIME } });
   const res = await response.json();
   const post = res as Post;
   return post;
 }
 
 export async function getAllPosts(): Promise<Post[]> {
-  const response = await fetch(`${ANDYLILFS_BLOG_BACKEND_API_URL}/blogs`, { cache: 'no-store' });
+  const response = await fetch(`${ANDYLILFS_BLOG_BACKEND_API_URL}/blogs`, { next: { revalidate: REVALIDATE_TIME } });
   const res = await response.json();
   const posts = res as Post[];
   return posts;
